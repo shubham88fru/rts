f6204ccd2ee7596266808f2f3a415fb80fab8800
Shubham Singh <shubham88foru@gmail.com>
2021-08-15 14:05:04 +0530

    finalized react redux typescript mini project

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state: any) => state.repositories
+  );
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form action="">
-        <input />
+      <form onSubmit={onSubmit} action="">
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error &&
+        !loading &&
+        data.map((name: any) => <div key={name}>{name}</div>)}
     </div>
   );
 };
